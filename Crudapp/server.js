e1339a5c4765af2377be1fd8b43e3a3d591749cb
farooqmajeed <farooq306@hotmail.com>
2017-06-09 16:15:52 +0500

    Crud app using Nodejs mongodb

--- Crudapp/server.js
+++ Crudapp/server.js
@@ -1,16 +1,21 @@
 // grab our Dependencies
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 8080;
+    port = process.env.PORT || 8080,
+    expressLayouts = require('express-ejs-layouts');
 
 
 //  configure our App
+app.use(express.static(__dirname + '/public'));
+
+// set ejs as our templeting engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts); 
 
 //  set the Routes
-    app.get('/',(req, res) => {
-        res.send('Hello, i am The App')
-    });
+app.use(require('./app/routes'))
+    
 
     app.listen(port, () => {
-        console.log('')
+        console.log('App Listening on https://localhost:${port}'); 
     });
