69d53962747b8b3cdfa7f71d98a172af12bf9d61
farooqmajeed <farooq306@hotmail.com>
2017-06-12 12:28:11 +0500

    Crud app using Nodejs mongodb

--- Crudapp/app/routes.js
+++ Crudapp/app/routes.js
@@ -1,11 +1,30 @@
 // creates a new router
 
 const express = require('express'),
- router = express.Router(),
- mainController = require('./contollers/main.controller');
+    router = express.Router(),
+    mainController = require('./contollers/main.controller'),
+    eventController = require('./contollers/events.controller');
 
 //  export Router
 module.exports = router;
 
 // define routes
-router.get('/', mainController.showHome)
+
+// main routes
+
+router.get('/', mainController.showHome);
+
+// event routes
+router.get('/events', eventController.showEvents); 
+
+
+// seed routes
+router.get('/events/seed', eventController.seedEvents)
+// create events
+
+// edit events
+
+// delete events
+
+// show a single events
+router.get('/events/:slug', eventController.showSingle);
