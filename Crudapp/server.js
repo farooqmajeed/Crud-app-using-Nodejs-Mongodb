8f9187a4be8db05f4332c59abf20f17b8b47a0de
farooqmajeed <farooq306@hotmail.com>
2017-06-13 15:02:03 +0500

    Crud app using Nodejs mongodb

--- Crudapp/server.js
+++ Crudapp/server.js
@@ -2,26 +2,44 @@ require('dotenv').config();
 
 
 // grab our Dependencies
-const express      = require('express'),
+    const express  = require('express'),
     app            = express(),
     port           = process.env.PORT || 8080,
-    expressLayouts = require('express-ejs-layouts');
-    mongoose       = require('mongoose');
+    expressLayouts = require('express-ejs-layouts'),
+    mongoose       = require('mongoose'),
+    session        = require('express-session'),
+    cookieParser   = require('cookie-parser'),
+    flash          = require('flash')
+    bodyParser     = require('body-parser');
 
 
 //  configure our App
 app.use(express.static(__dirname + '/public'));
 
-// set ejs as our templeting engine
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRET,
+    cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: false
+}));
+
+// flash
+app.use(flash());
+
+// set ejs as our templeting engine 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
-mongoose.connect(process.env. DB_URI)
+mongoose.connect(process.env.DB_URI)
+
+// use body parser to grab info from a form
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //  set the Routes
 app.use(require('./app/routes'))
 
 
 app.listen(port, () => {
-    console.log('App Listening on https://localhost:${port}');
+    console.log(`App Listening on https://localhost:${port}`);
 }); 
